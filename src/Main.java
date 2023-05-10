377f484f4bde8ac789fc112b6810a7e0f37c911b
Neto test <audov@mail.ru>
2023-05-10 09:33:33 +0300

    camelcase is applied

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
        public static void main(String[] args) {
-        int ticket_price = 32500; // стоимость билета
-        int mile_cost = 20; // стоимость мили в рублях
-        int mile_quantity = ticket_price / mile_cost; // количество начисленных миль
+        int ticketPrice = 32500; // стоимость билета
+        int mileCost = 20; // стоимость мили в рублях
+        int mileQuantity = ticketPrice / mileCost; // количество начисленных миль
 
         System.out.println("Количество бонусных миль за покупку билета составило:");
-        System.out.println(mile_quantity + " бонусных миль");
-      
+        System.out.println(mileQuantity + " бонусных миль");
+
     }
 }
